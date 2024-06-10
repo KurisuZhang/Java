00e68b9f5c71b05feccea736c1a68bb5906092ae
Shuai Zhang <33788131+KurisuZhang@users.noreply.github.com>
2024-06-10 18:59:53 +0000

    7

--- Assignment7/src/minDeleteToPalindrome.java
+++ Assignment7/src/minDeleteToPalindrome.java
@@ -17,8 +17,27 @@ public class minDeleteToPalindrome {
         return dp[0][n - 1];
     }
 
+    private static void test(String s, int expected) {
+        int result = minDeletionsToPalindrome(s);
+        if (result == expected) {
+            System.out.println("Test passed for input: " + s);
+        } else {
+            System.out.println("Test failed for input: " + s + ". Expected: " + expected + ", but got: " + result);
+        }
+    }
+
     public static void main(String[] args) {
         String s = "aebcbda";
         System.out.println("Minimum deletions: " + minDeletionsToPalindrome(s));
+        test("aebcbda", 2);  // 删除 'e' 和 'd' 或 'a' 和 'a'
+        test("abca", 1);     // 删除 'c' 或 'b'
+        test("racecar", 0);  // 已经是回文
+        test("abcdef", 5);   // 删除 'a', 'b', 'c', 'd', 'e' 或者 'b', 'c', 'd', 'e', 'f'
+        test("madam", 0);    // 已经是回文
+        test("aabb", 2);     // 删除两个 'a' 或两个 'b'
+        test("abcdeca", 2);  // 删除 'b', ', 'e' 或其他组合acdca
+        test("abcd", 3);     // 删除 'a', 'b', 'c' 或其他组合
+        test("a", 0);        // 单个字符已经是回文
+        test("", 0);         // 空字符串已经是回文
     }
 }

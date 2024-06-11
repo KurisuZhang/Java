05980b6e6c375478788b4965bec4b86f616d6316
Shuai Zhang <33788131+KurisuZhang@users.noreply.github.com>
2024-06-11 17:43:40 +0000

    7

---
+++ Assignment7/src/ReverseSentence.java
@@ -0,0 +1,22 @@
+public class ReverseSentence {
+    
+    public static void main(String[] args) {
+        String s = "i love programming very much";
+        String output = reverseSentence(s);
+        System.out.println(output);
+    }
+
+    public static String reverseSentence(String s) {
+        String[] strArray = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = strArray.length - 1; i >= 0; i--) {
+            sb.append(strArray[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+}

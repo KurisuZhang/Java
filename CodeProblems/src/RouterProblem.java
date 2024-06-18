d2fbcb7b34a956e13f3890634e21e939284ca361
Shuai Zhang <33788131+KurisuZhang@users.noreply.github.com>
2024-06-18 16:32:32 +0000

    code

--- CodeProblems/src/RouterProblem.java
+++ CodeProblems/src/RouterProblem.java
@@ -3,6 +3,8 @@ import java.util.Map;
 
 public class RouterProblem {
 
+
+    // O(M*N)
     public static int getServedBuildings(int[] buildingCount, int[] routerLocation, int[] routerRange) {
         int n = buildingCount.length;
         Map<Integer, Integer> coverageMap = new HashMap<>();
@@ -37,12 +39,53 @@ public class RouterProblem {
         return servedBuildings;
     }
 
+
+    // O(M+N)
+    public static int getServedBuildingsDifferenceArray(int[] buildingCount, int[] routerLocation, int[] routerRange) {
+        int n = buildingCount.length;
+        int[] coverage = new int[n + 1];
+
+        // Use a difference array to mark the ranges
+        for (int j = 0; j < routerLocation.length; j++) {
+            int start = Math.max(0, routerLocation[j] - routerRange[j] - 1);
+            int end = Math.min(n - 1, routerLocation[j] + routerRange[j] - 1);
+
+            coverage[start]++;
+            if (end + 1 < n) {
+                coverage[end + 1]--;
+            }
+        }
+
+        // Use prefix sum to get the actual coverage count for each building
+        int[] actualCoverage = new int[n];
+        actualCoverage[0] = coverage[0];
+        for (int i = 1; i < n; i++) {
+            actualCoverage[i] = actualCoverage[i - 1] + coverage[i];
+        }
+
+        // Count the number of served buildings
+        int servedBuildings = 0;
+        for (int i = 0; i < n; i++) {
+            if (actualCoverage[i] >= buildingCount[i]) {
+                servedBuildings++;
+            }
+            System.out.print("Building: " + (i + 1) + "   ");
+            System.out.print("Router Count: " + actualCoverage[i] + "   ");
+            System.out.print("Tenant Count: " + buildingCount[i] + "   ");
+            System.out.println("Served: " + (actualCoverage[i] >= buildingCount[i] ? "Yes" : "No"));
+        }
+
+        return servedBuildings;
+    }
+
+
     public static void main(String[] args) {
         int[] buildingCount = {1, 2, 1, 2, 2};
         int[] routerLocation = {3, 1};
         int[] routerRange = {1, 2};
 
         int result = getServedBuildings(buildingCount, routerLocation, routerRange);
+        // int result1 = getServedBuildingsDifferenceArray(buildingCount, routerLocation, routerRange);
         System.out.println("result: " + result);
     }
 }
